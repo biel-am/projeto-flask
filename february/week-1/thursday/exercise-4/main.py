bd2249ebf928386a9c3427bd85dae2facd022e72
Gabriel DBA <gabialexandremedeiros@gmail.com>
2025-02-07 11:47:37 -0300

    commit markdown

--- february/week-1/thursday/exercise-4/main.py
+++ february/week-1/thursday/exercise-4/main.py
@@ -1,3 +1,10 @@
+"""
+Exercise 4: All Films List
+Difficulty: Intermediate
+Description: Create a code to list all films in DataBase
+Expected Output: All films listed
+"""
+
 import psycopg2
 
 connection = psycopg2.connect(host='localhost', database='postgres', user='postgres', password='181044')
